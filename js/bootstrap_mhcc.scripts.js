320c6f39228977b1de367d714b1d1f116fd7c574
albaparticle <palagpatski@gmail.com>
2022-08-29 23:11:33 +0800

    Change selector

--- js/bootstrap_mhcc.scripts.js
+++ js/bootstrap_mhcc.scripts.js
@@ -1,118 +1,119 @@
 (function ($, Drupal) {
-    /**
-     * Toolbar Spacing
-     */
-    Drupal.behaviors.toolbarSpace = {
-        attach: function (context, settings) {
-            function toolbarSpace() {
-                toolbarHeight = $("#toolbar-bar").height();
-                trayHeight = $(".toolbar-tray-horizontal.is-active").length
-                    ? $(".toolbar-tray-horizontal.is-active").height()
-                    : 0;
-                topPadding = toolbarHeight + trayHeight;
+	/**
+	 * Toolbar Spacing
+	 */
+	Drupal.behaviors.toolbarSpace = {
+		attach: function (context, settings) {
+			function toolbarSpace() {
+				toolbarHeight = $("#toolbar-bar").height();
+				trayHeight = $(".toolbar-tray-horizontal.is-active").length
+					? $(".toolbar-tray-horizontal.is-active").height()
+					: 0;
+				topPadding = toolbarHeight + trayHeight;
 
-                $("body").css({
-                    paddingTop: topPadding,
-                });
-            }
+				$("body").css({
+					paddingTop: topPadding,
+				});
+			}
 
-            $(window).resize(toolbarSpace);
-        },
-    };
+			$(window).resize(toolbarSpace);
+		},
+	};
 
-    /**
-     * jquery UI dialog box close button fix
-     */
-    Drupal.behaviors.dialogCloseButtonFix = {
-        attach: function (context, settings) {
-            if ($.fn.button.noConflict) {
-                $.fn.bootstrapBtn = $.fn.button.noConflict();
-            }
-        },
-    };
+	/**
+	 * jquery UI dialog box close button fix
+	 */
+	Drupal.behaviors.dialogCloseButtonFix = {
+		attach: function (context, settings) {
+			if ($.fn.button.noConflict) {
+				$.fn.bootstrapBtn = $.fn.button.noConflict();
+			}
+		},
+	};
 
-    /**
-     * Make tables responsive
-     */
-    Drupal.behaviors.responsiveTables = {
-        attach: function (context, settings) {
-            $("table", context)
-                .not(".fc-content-skeleton > table")
-                .each(function () {
-                    $(this)
-                        .addClass("table")
-                        .wrap('<div class="table-responsive"></div>');
-                });
-        },
-    };
+	/**
+	 * Make tables responsive
+	 */
+	Drupal.behaviors.responsiveTables = {
+		attach: function (context, settings) {
+			$("table", context)
+				.not(".fc-content-skeleton > table")
+				.each(function () {
+					$(this)
+						.addClass("table")
+						.wrap('<div class="table-responsive"></div>');
+				});
+		},
+	};
 
-    /**
-     * Make Post Header adaptive
-     */
-    Drupal.behaviors.adaptivePostHeader = {
-        attach: function (context, settings) {
-            $(".post-header")
-                .has(".author-picture img")
-                .addClass("has-author-picture");
-        },
-    };
+	/**
+	 * Make Post Header adaptive
+	 */
+	Drupal.behaviors.adaptivePostHeader = {
+		attach: function (context, settings) {
+			$(".post-header")
+				.has(".author-picture img")
+				.addClass("has-author-picture");
+		},
+	};
 
-    /**
-     * Fullcalendar contrast text
-     */
-    Drupal.behaviors.ContrastText = {
-        attach: function (context, settings) {
-            // Helper function to check color contrast
-            const getContrast = (hexcolor) => {
-                // If a leading # is provided, remove it
-                if (hexcolor.slice(0, 1) === "#") {
-                    hexcolor = hexcolor.slice(1);
-                }
+	/**
+	 * Fullcalendar contrast text
+	 */
+	Drupal.behaviors.ContrastText = {
+		attach: function (context, settings) {
+			// Helper function to check color contrast
+			const getContrast = (hexcolor) => {
+				// If a leading # is provided, remove it
+				if (hexcolor.slice(0, 1) === "#") {
+					hexcolor = hexcolor.slice(1);
+				}
 
-                // Convert to RGB value
-                let r = parseInt(hexcolor.substr(0, 2), 16);
-                let g = parseInt(hexcolor.substr(2, 2), 16);
-                let b = parseInt(hexcolor.substr(4, 2), 16);
+				// Convert to RGB value
+				let r = parseInt(hexcolor.substr(0, 2), 16);
+				let g = parseInt(hexcolor.substr(2, 2), 16);
+				let b = parseInt(hexcolor.substr(4, 2), 16);
 
-                // Get YIQ ratio
-                let yiq = (r * 299 + g * 587 + b * 114) / 1000;
+				// Get YIQ ratio
+				let yiq = (r * 299 + g * 587 + b * 114) / 1000;
 
-                // Check contrast
-                return yiq >= 128 ? "black" : "white";
-            };
+				// Check contrast
+				return yiq >= 128 ? "black" : "white";
+			};
 
-            // Helper function to convert rgba to hex
-            const rgba2hex = (rgba) =>
-                `#${rgba
-                    .match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/)
-                    .slice(1)
-                    .map((n, i) =>
-                        (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n))
-                            .toString(16)
-                            .padStart(2, "0")
-                            .replace("NaN", "")
-                    )
-                    .join("")}`;
+			// Helper function to convert rgba to hex
+			const rgba2hex = (rgba) =>
+				`#${rgba
+					.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/)
+					.slice(1)
+					.map((n, i) =>
+						(i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n))
+							.toString(16)
+							.padStart(2, "0")
+							.replace("NaN", "")
+					)
+					.join("")}`;
 
-            const observer = new MutationObserver(function (mutations, observer) {
-                // fired when a mutation occurs
-                console.log(mutations, observer);
-                $('.fc-event', context).each(function() {
-                    let bgColor = $(this).css( "background-color" );
-                    let hexcolor = rgba2hex(bgColor);
-    
-                    $(this).find('.fc-content').css("color", getContrast(hexcolor));
-                });
-            });
+			const observer = new MutationObserver(function (mutations, observer) {
+				// fired when a mutation occurs
+				$(".fc-event", context).each(function () {
+					let bgColor = $(this).css("background-color");
+					let hexcolor = rgba2hex(bgColor);
 
-            const elementToObserve = document.querySelector("#main");
+					$(this).find(".fc-content").css("color", getContrast(hexcolor));
+				});
+			});
 
-            $(window).on("load", function () {
-                observer.observe(elementToObserve, {
-                    subtree: true,
-                    attributes: true,
-                });
-            });
-        },
-    };
+			const elementToObserve = document.querySelector("#fullcalendar-view");
+
+			$(window).on("load", function () {
+				if (elementToObserve) {
+					observer.observe(elementToObserve, {
+						subtree: true,
+						attributes: true,
+					});
+				}
+			});
+		},
+	};
 })(jQuery, Drupal);
